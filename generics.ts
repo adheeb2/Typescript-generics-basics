d5c39c51bb47a58a298d984f051ba4ed3da9cae2
adheeb2 <adheebanvar2@gmail.com>
2025-10-20 12:35:16 +0530

    feat(generics):initial basic commit

---
+++ generics.ts
@@ -0,0 +1,8 @@
+function getFirstElement(array: any[]) {
+  return array[0];
+}
+const numbers = [1, 2, 3];
+const firstElement = getFirstElement(numbers);
+
+const strings = ["sdf", "ssddffdg"];
+const firststring = getFirstElement(strings);
