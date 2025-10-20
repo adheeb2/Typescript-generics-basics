0f98df4ebdbcdb08c69e220191286c13b9778096
adheeb2 <adheebanvar2@gmail.com>
2025-10-20 12:42:57 +0530

    feat(generics):Add comment to explain generic function in generics.ts

--- generics.ts
+++ generics.ts
@@ -1,4 +1,5 @@
 //A generic function in TypeScript is a function that can work with various data types while maintaining type safety
+//Generic function makes typescript to know what the type of input data is without us writing the type,say for eg:array:Number[] can only give number as input and not strings and vice versa. but if we give generic functions, typescript will check on the type of the input and will give that as the data type
 function getFirstElement<ElementType>(array: ElementType[]) {
   return array[0];
 }
